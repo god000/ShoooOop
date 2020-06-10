008e5c2259d6c256dabe7f42cf95159572d03748
god000 <workalone000@gmai.com>
2020-06-10 11:00:05 +0300

    ...

---
+++ Storage/DataContext.cs
@@ -0,0 +1,39 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using Storage.Models;
+
+namespace Storage
+{
+    public class DataContext : DbContext
+    {
+
+        public DbSet<IBranch> Branches { get; set; }
+        public DbSet<IItem> Items { get; set; }
+
+        public void AddItem(IItem i)
+        {
+                Items.Add(i);
+                SaveChanges();
+        }
+
+        public void DeleteItem(IItem i)
+        {
+            SaveChanges();
+        }
+        public void SetInfo(object a)
+        {
+            Entry(a).State = EntityState.Modified;
+            SaveChanges();
+        }
+        public IBranch GetBranches(int id)
+        {
+            return Branches.Where(x => x.Id == id).FirstOrDefault();
+        }
+        
+        public IItem GetItem(int id)
+        {
+            return Items.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+    }
+}
---
+++ Storage/StorageService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using Storage.Models;
+
+namespace Storage
+{
+    public class StorageService : DropCreateDatabaseIfModelChanges<DbContext>, IStorageService
+    {
+        private DataContext db;
+        public DataContext Db => db;
+
+        
+
+        public StorageService()
+        {
+            db = new DataContext();            
+        }
+
+                
+        public void SetInfo(object obj)
+        {
+            db.SetInfo(obj);
+        }
+        
+
+        public void AddItem(IItem i)
+        {
+            db.AddItem(i);
+        }
+
+        public void DeleteItem(IItem i)
+        {
+            db.DeleteItem(i);
+        }
+
+        public bool CheckCanAddItem(IItem i, string category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IItem GetItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<IItem> GetItemFromBranche(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
