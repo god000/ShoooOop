0a8dabd8fbb300788c2456b2cea1ed678d4310f9
god000 <workalone000@gmai.com>
2020-06-10 09:53:59 +0300

    added item model

---
+++ Storage/Models/IItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Storage.Models
+{
+    public interface IItem
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        string Category { get; set; }
+        int PriseBuy { get; set; }
+        int PriseSell { get; set; }
+        int dateProduction { get; set; }
+        int ShelfLife { get; set; }
+        string Type { get; set; }
+    }
+}
---
+++ Storage/Models/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.Entity;
+
+namespace Storage.Models
+{
+    public class Item : IItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get ; set ; }
+        public int PriseBuy { get ; set; }
+        public int PriseSell { get ; set ; }
+        public int dateProduction { get ; set ; }
+        public int ShelfLife { get; set ; }
+        public string Type { get ; set ; }
+    }
+}
